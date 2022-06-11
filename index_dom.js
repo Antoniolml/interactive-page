b672639ee77c88941ae1b2159e559cd27818c97d
Antoniolml <antoniopalaciosorihuela@gmail.com>
2022-06-11 15:34:34 -0500

    agregue evento del teclado atajos y movimientos y colisiones

--- index_dom.js
+++ index_dom.js
@@ -1,5 +1,7 @@
 import hamburgerMenu from "./dom/menu_hamburger.js";
 import {digitalClock, alarm} from "./dom/reloj.js";
+import { moveBall, shortCuts } from "./dom/teclado.js";
+
 
 
 const d = document; 
@@ -8,4 +10,10 @@ d.addEventListener("DOMContentLoaded", e => {
   hamburgerMenu(".panel-btn", ".panel", ".menu a")
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
   alarm("./assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma")
+});
+
+// shortCuts
+d.addEventListener("keydown", e => {
+  shortCuts(e);
+  moveBall(e,".ball", ".stage");
 });
