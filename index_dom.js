8f24725e80d9c547ce1535bf044efc0a402b5b78
Antoniolml <antoniopalaciosorihuela@gmail.com>
2022-06-12 10:05:54 -0500

    agregue cuenta regresiva

--- index_dom.js
+++ index_dom.js
@@ -1,15 +1,20 @@
 import hamburgerMenu from "./dom/menu_hamburger.js";
 import {digitalClock, alarm} from "./dom/reloj.js";
 import { moveBall, shortCuts } from "./dom/teclado.js";
-
+import countDown from "./dom/cuenta_regresiva.js";
+countDown
 
 
 const d = document; 
 
 d.addEventListener("DOMContentLoaded", e => {
-  hamburgerMenu(".panel-btn", ".panel", ".menu a")
-  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj")
-  alarm("./assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma")
+  hamburgerMenu(".panel-btn", ".panel", ".menu a");
+  digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
+  alarm("./assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countDown(
+    "countDown",
+    "Jun 11, 2022 17:39:00", 
+    "feliz cumple");
 });
 
 // shortCuts
