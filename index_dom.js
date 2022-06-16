186bc8a1e8ce934a38127903e7074cbe3c4ceaed
Antoniolml <antoniopalaciosorihuela@gmail.com>
2022-06-16 09:35:48 -0500

    agregue responsive design

--- index_dom.js
+++ index_dom.js
@@ -4,6 +4,7 @@ import { moveBall, shortCuts } from "./dom/teclado.js";
 import countDown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/botton_scroll.js";
 import darkTheme from "./dom/theme_dark.js";
+import responsiveMedia from "./dom/objeto_responsive.js";
 
 
 
@@ -17,9 +18,20 @@ d.addEventListener("DOMContentLoaded", e => {
     "countDown",
     "Dec 11, 2022 17:39:00", 
     "feliz cumple 🤑"
-  );
+    );
   scrollTopButton(".scroll_btn");
-  darkTheme(".dark_theme", "dark-mode");
+    
+  responsiveMedia("youtube",
+   "(min-width: 1024px)", 
+   `<a href="https://www.youtube.com/embed/6IwUl-4pAzc" target="_blank" rel = "noopener">Ver video</a>`, 
+   `<iframe width="560" height="315" src="https://www.youtube.com/embed/6IwUl-4pAzc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+   );
+
+   responsiveMedia("gmaps",
+   "(min-width: 1024px)", 
+   `<a href="https://goo.gl/maps/pitFRZkUKiXZZUpW9" target="_blank" rel = "noopener">Ver video</a>`, 
+   `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.881416470793!2d-77.0367842490311!3d-12.05167924523277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c9d7b6a104f1%3A0xf90de36df50e9438!2sPlaza%20San%20Martin!5e0!3m2!1ses-419!2spe!4v1655308657866!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
+   );
 });
 
 // shortCuts
@@ -27,3 +39,5 @@ d.addEventListener("keydown", e => {
   shortCuts(e);
   moveBall(e,".ball", ".stage");
 });
+
+darkTheme(".dark_theme", "dark-mode");
