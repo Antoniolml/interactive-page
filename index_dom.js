f423b075d55a57237098f641c21718d8f25216ef
Antoniolml <antoniopalaciosorihuela@gmail.com>
2022-06-18 15:19:28 -0500

    Agregue deteccion de estado de red

--- index_dom.js
+++ index_dom.js
@@ -5,6 +5,9 @@ import countDown from "./dom/cuenta_regresiva.js";
 import scrollTopButton from "./dom/botton_scroll.js";
 import darkTheme from "./dom/theme_dark.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
+import responsiveTester from "./dom/prueba_responsive.js";
+import userDeviceInfo from "./dom/deteccion_dispositivos.js";
+import networkStatus from "./dom/deteccion_red.js";
 
 
 
@@ -32,6 +35,11 @@ d.addEventListener("DOMContentLoaded", e => {
    `<a href="https://goo.gl/maps/pitFRZkUKiXZZUpW9" target="_blank" rel = "noopener">Ver video</a>`, 
    `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3901.881416470793!2d-77.0367842490311!3d-12.05167924523277!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c9d7b6a104f1%3A0xf90de36df50e9438!2sPlaza%20San%20Martin!5e0!3m2!1ses-419!2spe!4v1655308657866!5m2!1ses-419!2spe" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`
    );
+
+   responsiveTester("responsive-tester");
+   userDeviceInfo("user-device")
+
+
 });
 
 // shortCuts
@@ -41,3 +49,5 @@ d.addEventListener("keydown", e => {
 });
 
 darkTheme(".dark_theme", "dark-mode");
+
+networkStatus();
